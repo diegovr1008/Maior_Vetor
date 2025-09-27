1158744c721281fcb281549182bb0c45960b1d6c
diegovr1008 <diego.vinicius1008@gmail.com>
2025-09-26 21:42:34 -0300

    Adicionar cada nova posição no vetor Posicção

--- maior_vetor_posicao.py
+++ maior_vetor_posicao.py
@@ -1,16 +1,18 @@
-numeros = [4.5, 2, 9.0, 7, 9.8]
+numeros = [100, 2, 100, 7, 9, 100]
 
 def maior_num(vetor):
     if not vetor:
         return None
     
     maior = vetor[0]
-    posicao = 0
+    posicao = []
 
     for i in range(len(vetor)):
         if vetor[i] > maior:
             maior = vetor[i]
-            posicao = i
+            posicao = [i]
+        elif vetor[i] == maior:
+            posicao.append(i)
     return (maior, posicao)
 
 print(maior_num(numeros))
